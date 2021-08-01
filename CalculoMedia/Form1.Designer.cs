e733a6433d237fede1301f654b5133707d644e10
mlukc <mlukc@DESKTOP-HH2RHUG>
2021-08-01 18:32:48 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoMedia/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace CalculoMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nota2 = new System.Windows.Forms.TextBox();
+            this.nota3 = new System.Windows.Forms.TextBox();
+            this.txtmedia = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nota1 = new System.Windows.Forms.TextBox();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo Média";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primeira Nota";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Segunda Nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Terceira Nota";
+            // 
+            // nota2
+            // 
+            this.nota2.Location = new System.Drawing.Point(193, 192);
+            this.nota2.Name = "nota2";
+            this.nota2.Size = new System.Drawing.Size(200, 22);
+            this.nota2.TabIndex = 2;
+            // 
+            // nota3
+            // 
+            this.nota3.Location = new System.Drawing.Point(193, 245);
+            this.nota3.Name = "nota3";
+            this.nota3.Size = new System.Drawing.Size(200, 22);
+            this.nota3.TabIndex = 2;
+            // 
+            // txtmedia
+            // 
+            this.txtmedia.Location = new System.Drawing.Point(193, 394);
+            this.txtmedia.Name = "txtmedia";
+            this.txtmedia.Size = new System.Drawing.Size(200, 22);
+            this.txtmedia.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 387);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Sua média é: ";
+            // 
+            // nota1
+            // 
+            this.nota1.Location = new System.Drawing.Point(193, 137);
+            this.nota1.Name = "nota1";
+            this.nota1.Size = new System.Drawing.Size(200, 22);
+            this.nota1.TabIndex = 2;
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedia.Location = new System.Drawing.Point(31, 304);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(139, 53);
+            this.btnMedia.TabIndex = 3;
+            this.btnMedia.Text = "Calcular Média";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(193, 304);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(139, 53);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(360, 304);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(139, 53);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(529, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtmedia);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.nota3);
+            this.Controls.Add(this.nota1);
+            this.Controls.Add(this.nota2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nota2;
+        private System.Windows.Forms.TextBox nota3;
+        private System.Windows.Forms.TextBox txtmedia;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nota1;
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ CalculoMedia/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, media;
+            n1 = Convert.ToDouble(nota1.Text);
+            n2 = Convert.ToDouble(nota2.Text);
+            n3 = Convert.ToDouble(nota3.Text);
+            media = (n1 + n2 + n3) / 3;
+            txtmedia.Text = media.ToString();
+
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            nota1.Text = "";
+            nota2.Text = "";
+            nota3.Text = "";
+            txtmedia.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+    }
+}
